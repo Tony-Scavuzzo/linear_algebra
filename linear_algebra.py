8b5288dc69db2e0ad05f3a7ee8e147c48691e853
Tony Scavuzzo <ar.scavuzzo@gmail.com>
2023-03-17 02:15:44 -0500

    Began process of generalizing vectors to matrices

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,8 +1,67 @@
 import math
 
 #these functions are general purpose for this code
-def square_matrix_maker():
-    #this function interactively sets up a square matrix
+def get_dimensions(matrix):
+    #returns a tuple describing the dimensionality of a matrix
+    return(len(matrix),len(matrix[0]))
+
+def is_vector(matrix):
+    #determines if a matrix is a vector and if it is a vector, what type ("row" or "column")
+    #Returns a tuple of the form (bool, "type")
+    dimensions = get_dimensions(matrix)
+
+    if dimensions[0] == 1:
+        return (True, "row")
+
+    elif dimensions[1] == 1:
+        return (True, "column")
+
+    else:
+        return (False, None)
+
+def is_square_matrix(matrix):
+    #determines if a matrix is a square matrix and if it is, what the dimensionality is
+    #returns a tuple of the form (bool, dimensions)
+
+    dimensions = get_dimensions(matrix)
+
+    if dimensions[0] == dimensions[1]:
+        return (True, dimensions[0])
+
+    else:
+        return (False, None)
+
+def matrix_maker():
+    #this function interactively sets up a matrix
+    #here, the dimensionality is established
+    dimensions_done = False
+    while(dimensions_done == False):
+        dimension_string = input("   What is the dimensionality of your matrix?\n   Enter as <#rows>,<#columns>.\n>")
+        dimensions = dimension_string.split(",")
+        if len(dimensions) == 2:
+            i = 0
+            while i < 2:
+                dimensions[i] = int(dimensions[i].strip())
+                i = i + 1
+            dimensions_done = True
+        else:
+            print("A matrix has two dimensions - try again")
+        
+    
+    #here, the rows of the matrix are input one at a time
+    matrix = []
+    while len(matrix) < dimensions[0]:
+        row = clean_vector(input(f"   Input the values for row {len(matrix)+1} as a comma seperated list without brackets.\n>"))
+        if len(row) == dimensions[1]:
+            matrix.append(row)
+        else:
+            print("Row was not the expected size. Try again")
+    
+    return matrix
+
+""" def square_matrix_maker():
+    #this function has been depreciated because I have generalized to matrix_maker()
+    #this function interactively sets up a square mat1rix
     order = int(input("   What is the size of your square matrix?\n   Enter an integer.\n>"))
     matrix = []
     while len(matrix) < order:
@@ -11,7 +70,7 @@ def square_matrix_maker():
             matrix.append(row)
         else:
             print("Row was not the expected size. Try again")
-    return(matrix)
+    return(matrix) """
 
 def clean_vector(vector_input):
     #this function takes a user input vector as a comma seperated string and converts it to a list while cleaning spaces.
@@ -22,11 +81,28 @@ def clean_vector(vector_input):
         i = i + 1
     return vector
 
-def check_dimensions(vector1, vector2):
+def check_vector_dim(vector1, vector2):
     #this function returns an error if two given vectors are of different dimension
     if len(vector1) != len(vector2):
         print("   Error: This operation is only defined for two vectors of equal order.")
         exit()
+
+def matrix_printer(matrix):
+    #formats a matrix as a string with line jumps
+    outstring = ""
+    i = 0
+    if len(matrix) == 1:
+        outstring = "   [" + str(matrix[i]) + "]\n"
+    else:
+        while i < len(matrix):
+            if i == 0:
+                outstring = outstring + "   [" + str(matrix[i]) + "\n"
+            elif i == len(matrix) - 1:
+                outstring = outstring + "    " + str(matrix[i]) + "]\n"
+            else:
+                outstring = outstring + "    " + str(matrix[i]) + "\n"
+            i = i + 1
+    return outstring
 
 def output(string):
     #formats outputs consistently
@@ -97,22 +173,67 @@ def magnitude(vector):
         sum_of_squares = sum_of_squares + element * element
     return math.sqrt(sum_of_squares)
 
-def vector_add(vector1, vector2):
-    #adds two vectors together
-    vector_sum = []
+def transpose(matrix):
+    dimensions = get_dimensions(matrix)
+    new_matrix = []
+    #indeces traverse new matrix, not old matrix
     i = 0
-    while i < len(vector1):
-        vector_sum.append(vector1[i]+vector2[i])
+    while i < dimensions[1]:
+        new_matrix.append([])
+        j = 0
+        while j < dimensions[0]:
+            new_matrix[i].append(matrix[j][i])
+            j = j + 1
         i = i + 1
-    return vector_sum
+    return(new_matrix)
 
-def scalar_vector_multiply(scalar, vector):
+def matrix_add(matrix1, matrix2):
+    #adds two matrices together
+    dimensions = get_dimensions(matrix1)
+    matrix_sum = []
     i = 0
-    new_vector = []
-    while i < len(vector):
-        new_vector.append(scalar*vector[i])
+    while i < dimensions[0]:
+        matrix_sum.append([])
+        j = 0
+        while j < dimensions[1]:
+            matrix_sum[i].append(matrix1[i][j]+matrix2[i][j])
+            j = j + 1
         i = i + 1
-    return(new_vector)
+    return matrix_sum
+
+def scalar_matrix_multiply(scalar, matrix):
+    dimensions = get_dimensions(matrix)
+    i = 0
+    new_matrix = []
+    while i < dimensions[0]:
+        new_matrix.append([])
+        j = 0
+        while j < dimensions[1]:
+            new_matrix[i].append(scalar * matrix[i][j])
+            j = j + 1
+        i = i + 1
+    return(new_matrix)
+
+def matrix_matrix_multiply(matrix1, matrix2):
+    dimensions1 = get_dimensions(matrix1)
+    dimensions2 = get_dimensions(matrix2)
+    new_matrix = []
+    #indices i and j traverse the elements of the new matrix
+    i = 0
+    while i < dimensions1[0]:
+        new_matrix.append([])
+        j = 0
+        while j < dimensions2[1]:
+            #index k traverses the original matrices
+            element = 0
+            k = 0
+            while k < dimensions1[1]:
+                element = element + matrix1[i][k] * matrix2[k][j]
+                k = k + 1
+            new_matrix[i].append(element)
+            j = j + 1
+        i = i + 1
+    return(new_matrix)
 
 def dot_product(vector1, vector2):
     #finds the dot product of two vectors
@@ -195,7 +316,7 @@ while True:
 
     #interactive text, includuing menu
     print("   Please select an operation from the following menu by typing a number and pressing enter.")
-    print("   0: exit\n   1: vector magnitude\n   2: vector addition\n   3: multiply vector by scalar\n   4: dot product\n   5: find angle between vectors\n   6: cross product\n   7: matrix determinant")  
+    print("   0: exit\n   1: vector magnitude\n   2: matrix transpose\n   3: matrix addition\n   4: multiply matrix by scalar\n   5: multiply matrix by matrix\n   6: find angle between vectors\n   7: cross product\n   8: matrix determinant\n   -1: test code")  
     #changes menu to reflect degrees/radians
     if degree_switch == True:
         print('\n   you are currently working in degrees. To switch to radians, enter "r."')
@@ -212,49 +333,76 @@ while True:
         #vector magnitude
         vector = clean_vector(input("   Input a vector as a comma seperated list without brackets:\n>"))
         output(f"The magnitude of {vector} is {magnitude(vector)}.")
-        
+
     elif choice == "2":
-        #add vectors
-        vector1 = clean_vector(input("   Input vector 1 as a comma seperated list without brackets:\n>"))
-        vector2 = clean_vector(input("   Input vector 2 as a comma seperated list without brackets:\n>"))
-        check_dimensions(vector1,vector2)
-        output(f"The sum of {vector1} and {vector2} is {vector_add(vector1,vector2)}")
-        
+        matrix = matrix_maker()
+        output(f"The transpose of\n{matrix_printer(matrix)}   is\n{matrix_printer(transpose(matrix))}")
+
     elif choice == "3":
-        #multiply vector by scalar
-        vector = clean_vector(input("   Input vector as a comma seperated list without brackets:\n>"))
-        scalar = float(input("   Input scalar:\n>"))
-        output(f"{scalar} * {vector} = {scalar_vector_multiply(scalar, vector)}")
-    
-    elif choice == "4":
-        #dot product
-        vector1 = clean_vector(input("   Input vector 1 as a comma seperated list without brackets:\n>"))
-        vector2 = clean_vector(input("   Input vector 2 as a comma seperated list without brackets:\n>"))
-        check_dimensions(vector1,vector2)
-        output(f"The dot product of vector 1 and vector 2 is {dot_product(vector1, vector2)}.")
+        #add matrices
+        print("   For matrix 1:")
+        matrix1 = matrix_maker()
+        print("   for matrix 2:")
+        matrix2 = matrix_maker()
         
+        if get_dimensions(matrix1) == get_dimensions(matrix2):
+            output(f"M1 =\n{matrix_printer(matrix1)}\n   M2 =\n{matrix_printer(matrix2)}\n   M1 + M2 =\n{matrix_printer(matrix_add(matrix1,matrix2))}")
+        else:
+            output("Matrix addition is not supported for matrices of different dimensionality")
+        #print(matrix_add(matrix1,matrix2))
+        #print(matrix_printer(matrix_add(matrix1,matrix2)))
+        
+    elif choice == "4":
+        #multiply matrix by scalar
+        matrix = matrix_maker()
+        scalar = float(input("   Input scalar:\n>"))
+        output(f"{scalar} *\n{matrix_printer(matrix)}   =\n{matrix_printer(scalar_matrix_multiply(scalar, matrix))}")
+    
     elif choice == "5":
+        #mutliply matrix by matrix
+        print("   For matrix 1:")
+        matrix1 = matrix_maker()
+        print("   for matrix 2:")
+        matrix2 = matrix_maker()
+
+        if get_dimensions(matrix1)[1] == get_dimensions(matrix2)[0]:
+            output(f"M1 =\n{matrix_printer(matrix1)}\n   M2 =\n{matrix_printer(matrix2)}\n   M1 * M2 =\n{matrix_printer(matrix_matrix_multiply(matrix1,matrix2))}")
+
+        elif get_dimensions(matrix1)[0] == get_dimensions(matrix2)[0] and get_dimensions(matrix1)[1] == 1 and get_dimensions(matrix2)[1] == 1:
+            if input("   The product of two column vectors is not defined. Did you intend to find the dot product? (y/n)\n>") == "y":
+                Tmatrix1 = transpose(matrix1)
+                output(f"M1 =\n{matrix_printer(matrix1)}\n   M2 =\n{matrix_printer(matrix2)}\n   The dot product of M1 and M2 is {matrix_matrix_multiply(Tmatrix1,matrix2)[0][0]}")
+        
+        elif get_dimensions(matrix1)[1] == get_dimensions(matrix2)[1] and get_dimensions(matrix1)[0] == 1 and get_dimensions(matrix2)[0] == 1:
+            if input("   The product of two row vectors is not defined. Did you intend to find the dot product? (y/n)\n>") == "y":
+                Tmatrix2 = transpose(matrix2)
+                output(f"M1 =\n{matrix_printer(matrix1)}\n   M2 =\n{matrix_printer(matrix2)}\n   The dot product of M1 and M2 is {matrix_matrix_multiply(matrix1,Tmatrix2)[0][0]}")
+        
+        else:
+            output("The product of two matrices of dimensions axb and cxd is not defined when b != c.")
+
+    elif choice == "6":
         #angle of two vectors
         vector1 = clean_vector(input("   Input vector 1 as a comma seperated list without brackets:\n>"))
         vector2 = clean_vector(input("   Input vector 2 as a comma seperated list without brackets:\n>"))
-        check_dimensions(vector1,vector2)
+        check_vector_dim(vector1,vector2)
         answer = find_angle(vector1, vector2)
         if degree_switch == True:
             answer = answer/math.pi*180
         output(f"The angle between vector 1 and vector 2 is {answer}.")
         
-    elif choice == "6":
+    elif choice == "7":
         #cross product
         vector1 = clean_vector(input("   Input vector 1 as a comma seperated list without brackets:\n>"))
         vector2 = clean_vector(input("   Input vector 2 as a comma seperated list without brackets:\n>"))
-        check_dimensions(vector1,vector2)
+        check_vector_dim(vector1,vector2)
         answer = cross_product(vector1, vector2)
         if answer == None:
             output("Error: The cross product is only defined in three dimensional space")
         else:
             output(f"The cross product of vector 1 and vector 2 is {answer}")
     
-    elif choice == "7":
+    elif choice == "8":
         #determinant solver
         
         matrix = square_matrix_maker()
@@ -266,6 +414,10 @@ while True:
         answer = answer + f"   is {determinant_solver(matrix)}"
         output(answer)
     
+    elif choice == "-1":
+        print("   This is the current test code.\n   If you are running this code, you should already know what it does.")
+        matrix1 = matrix_maker()
+        print(matrix_printer(transpose(matrix1)))
     elif choice == "r":
         degree_switch = False
     
